f1954ca9c7160a717ea2c2c8a3d7b08f21d71a8d
Willow Ahrens <ptrahrens@gmail.com>
2022-11-12 14:21:05 -0500

    test mistake

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test
     ex = :(f(a,b))
     @test operation(ex) == :call
     @test arguments(ex) == [:f, :a, :b]
-    @test ex == similarterm(ex, call, [:f, :a, :b])
+    @test ex == similarterm(ex, :call, [:f, :a, :b])
 
     ex = :(arr[i, j])
     @test operation(ex) == :ref
